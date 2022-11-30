397bd359f27076c74ac49d4fec6b02e90107f21b
Владимир Дворный <vladimirdvornyj97017@gmail.com>
2022-11-30 17:22:46 +0500

    IS-1-20-DvornyjVA-U

--- IS-1-20-DvornyjVA-U/Form1.Designer.cs
+++ IS-1-20-DvornyjVA-U/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace IS_1_20_DvornyjVA_U
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "Menu.cs";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
