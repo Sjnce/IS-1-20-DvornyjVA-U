207311280076365cb570a38d72c7ccd0765e574f
Владимир Дворный <vladimirdvornyj97017@gmail.com>
2022-12-02 19:10:16 +0500

    сделано 1 задание

--- IS-1-20-DvornyjVA-U/Task1.Designer.cs
+++ IS-1-20-DvornyjVA-U/Task1.Designer.cs
@@ -57,7 +57,7 @@ namespace IS_1_20_DvornyjVA_U
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(254, 8);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 78);
             this.button1.TabIndex = 0;
@@ -67,7 +67,7 @@ namespace IS_1_20_DvornyjVA_U
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(510, 12);
+            this.button2.Location = new System.Drawing.Point(12, 105);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 78);
             this.button2.TabIndex = 1;
@@ -77,7 +77,7 @@ namespace IS_1_20_DvornyjVA_U
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(762, 8);
+            this.button3.Location = new System.Drawing.Point(12, 198);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 78);
             this.button3.TabIndex = 2;
@@ -88,77 +88,77 @@ namespace IS_1_20_DvornyjVA_U
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(232, 218);
+            this.listBox1.Location = new System.Drawing.Point(200, 12);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(744, 264);
             this.listBox1.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(254, 99);
+            this.textBox1.Location = new System.Drawing.Point(200, 285);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(253, 125);
+            this.textBox2.Location = new System.Drawing.Point(199, 311);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(510, 96);
+            this.textBox3.Location = new System.Drawing.Point(504, 285);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(510, 122);
+            this.textBox4.Location = new System.Drawing.Point(504, 311);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 7;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(511, 148);
+            this.textBox5.Location = new System.Drawing.Point(505, 337);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 8;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(510, 174);
+            this.textBox6.Location = new System.Drawing.Point(504, 363);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 9;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(762, 96);
+            this.textBox7.Location = new System.Drawing.Point(725, 285);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
             this.textBox7.TabIndex = 10;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(763, 122);
+            this.textBox8.Location = new System.Drawing.Point(726, 311);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 20);
             this.textBox8.TabIndex = 11;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(762, 148);
+            this.textBox9.Location = new System.Drawing.Point(725, 337);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(100, 20);
             this.textBox9.TabIndex = 12;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(763, 174);
+            this.textBox10.Location = new System.Drawing.Point(726, 363);
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(100, 20);
             this.textBox10.TabIndex = 13;
@@ -166,7 +166,7 @@ namespace IS_1_20_DvornyjVA_U
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(361, 102);
+            this.label1.Location = new System.Drawing.Point(307, 288);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 14;
@@ -175,7 +175,7 @@ namespace IS_1_20_DvornyjVA_U
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(361, 128);
+            this.label2.Location = new System.Drawing.Point(307, 314);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 15;
@@ -184,7 +184,7 @@ namespace IS_1_20_DvornyjVA_U
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(617, 99);
+            this.label3.Location = new System.Drawing.Point(611, 288);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 16;
@@ -193,7 +193,7 @@ namespace IS_1_20_DvornyjVA_U
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(617, 125);
+            this.label4.Location = new System.Drawing.Point(611, 314);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 13);
             this.label4.TabIndex = 17;
@@ -202,7 +202,7 @@ namespace IS_1_20_DvornyjVA_U
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(617, 151);
+            this.label5.Location = new System.Drawing.Point(611, 340);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(91, 13);
             this.label5.TabIndex = 18;
@@ -211,7 +211,7 @@ namespace IS_1_20_DvornyjVA_U
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(616, 177);
+            this.label6.Location = new System.Drawing.Point(610, 366);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 13);
             this.label6.TabIndex = 19;
@@ -220,7 +220,7 @@ namespace IS_1_20_DvornyjVA_U
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(868, 99);
+            this.label7.Location = new System.Drawing.Point(831, 288);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(74, 13);
             this.label7.TabIndex = 20;
@@ -229,7 +229,7 @@ namespace IS_1_20_DvornyjVA_U
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(868, 125);
+            this.label8.Location = new System.Drawing.Point(831, 314);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(75, 13);
             this.label8.TabIndex = 21;
@@ -238,7 +238,7 @@ namespace IS_1_20_DvornyjVA_U
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(868, 151);
+            this.label9.Location = new System.Drawing.Point(831, 340);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(112, 13);
             this.label9.TabIndex = 22;
@@ -247,7 +247,7 @@ namespace IS_1_20_DvornyjVA_U
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(868, 177);
+            this.label10.Location = new System.Drawing.Point(831, 366);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(108, 13);
             this.label10.TabIndex = 23;
@@ -257,7 +257,7 @@ namespace IS_1_20_DvornyjVA_U
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 522);
+            this.ClientSize = new System.Drawing.Size(1020, 395);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
